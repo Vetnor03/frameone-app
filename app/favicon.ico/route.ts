5af331aee91253160de04268d5558df90cf2987c
Vetnor03 <86721503+Vetnor03@users.noreply.github.com>
2026-05-11 21:36:41 +0200

    Remove added icon binaries

--- app/favicon.ico/route.ts
+++ app/favicon.ico/route.ts
@@ -4,5 +4,5 @@ import { versionedIconPath } from '../lib/iconVersion'
 export const dynamic = 'force-static'
 
 export function GET(request: Request) {
-  return NextResponse.redirect(new URL(versionedIconPath('/favicon.svg'), request.url), 308)
+  return NextResponse.redirect(new URL(versionedIconPath('/favicon-32x32.png'), request.url), 308)
 }
