c1fd58bab04288134a948ff4048d757295d9a1b9
Vetnor03 <86721503+Vetnor03@users.noreply.github.com>
2026-07-09 22:04:51 +0200

    Fix manifest icon purpose typing

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: versionedIconPath('/icon.png'),
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
+      },
+      {
+        src: versionedIconPath('/icon.png'),
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
