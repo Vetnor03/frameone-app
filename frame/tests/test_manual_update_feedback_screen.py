60820efb9e1110b08e6426188f6172ea6e464007
Vetnor03 <86721503+Vetnor03@users.noreply.github.com>
2026-09-24 15:28:25 +0200

    Keep News firmware version at v2.7.2

--- frame/tests/test_manual_update_feedback_screen.py
+++ frame/tests/test_manual_update_feedback_screen.py
@@ -34,5 +34,5 @@ def test_updating_screen_is_full_frame_and_theme_aware():
     assert 'drawCenteredTextInFrame("Updating...", 1);' in body
 
 
-def test_firmware_version_tracks_current_feature_set():
-    assert 'static const char* FW_VER = "v2.8.0";' in INO
+def test_firmware_version_remains_2_7_2():
+    assert 'static const char* FW_VER = "v2.7.2";' in INO
